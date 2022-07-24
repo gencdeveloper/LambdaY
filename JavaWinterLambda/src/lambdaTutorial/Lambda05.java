fe62db6396681caf2dc8dcf2e5dfd6845c9e4f2c
Kirito <halid.gundz@gmail.com>
2022-07-24 14:57:22 +0200

    v1.0

---
+++ JavaWinterLambda/src/lambdaTutorial/Lambda05.java
@@ -0,0 +1,63 @@
+package lambdaTutorial;
+
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+
+public class Lambda05 {
+    public static void main(String[] args) {
+        System.out.println("TASK 01 -->");
+        System.out.println("   ***   ");
+
+        System.out.println("TASK 02 -->");
+        System.out.println("   ***   ");
+
+        System.out.println("TASK 03 -->");
+        System.out.println("   ***   ");
+
+        System.out.println("TASK 04 -->");
+        System.out.println("   ***   ");
+
+        System.out.println("TASK 05 -->");
+        System.out.println("   ***   ");
+
+        System.out.println("TASK 06 -->");
+        System.out.println("   ***   ");
+
+        System.out.println("TASK 07 -->");
+        System.out.println("   ***   ");
+
+        System.out.println("TASK 08 -->");
+        System.out.println("   ***   ");
+
+    }
+
+    //TASK 01 --> Structured Programming ve Functional Programming ile 1'den x'e kadar tamsayilari toplayan bir program create ediniz.
+
+    //Structured(AMELE) Programming
+
+
+    //Functional(cincix Programming
+
+
+    //TASK 02 --> 1'den x'e kadar cift tamsayilari toplayan bir program create ediniz.
+
+
+    //TASK 03 --> Ilk x pozitif cift sayiyi toplayan program  create ediniz.
+
+
+    //TASK 04 --> Ilk X pozitif tek tamsayiyi toplayan programi  create ediniz.
+
+
+    //TASK 05 --> 2'nin ilk x kuvvetini ekrana yazdiran programi  create ediniz.
+
+
+    //TASK 06 --> Istenilen bir sayinin ilk x kuvvetini ekrana yazdiran programi  create ediniz.
+
+
+    //TASK 07 --> Istenilen bir sayinin faktoriyelini hesaplayan programi  create ediniz.
+
+
+    //TASK 08 --> Istenilen bir sayinin  x. kuvvetini ekrana yazdiran programi  create ediniz.
+
+
+}
